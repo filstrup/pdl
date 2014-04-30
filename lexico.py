fb825e6dc935ba38dc61812f4a5f4402ca941ef3
filstrup <filstroop@gmail.com>
2014-04-30 16:21:23 +0200

    agsGADRGDAGTWfsgaw

--- lexico.py
+++ lexico.py
@@ -10,4 +10,24 @@ ALL = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q',
        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z',
        '0','1','2','3','4','5','6','7','8','9',
        '+','-','*','/','%','&','|','!','<','>','=','(',')','[',']','{','}',';',',',':','.','?','\n','\t','\0','\b','EOF','EOL']
-       
+    
+    
+#Al analizador le vas pasando letras, él se encarga de ir concatenando y/o creando tokens
+def anaizador_lexico(char):
+    return
+
+
+with open("/home/filstrup/hub/pdl/Ejem.js") as f: #abrimos el archivo
+        c = f.read().splitlines(1)
+        texto = ""
+        for i in c:
+            texto+=i
+        print texto
+        
+        with open("output.txt", "w") as text_file:
+            for i in c:
+                text_file.write(i)
+                
+        for i in c:
+            for letra in i:
+                analizador_lexico(letra)
